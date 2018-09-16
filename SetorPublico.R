5adfc142f05c5e3816f4fcd3ed24a4946f51d2eb
portugalexandre <43226664+portugalexandre@users.noreply.github.com>
2018-09-15 22:24:19 -0300

    Update SetorPublico.R

--- SetorPublico.R
+++ SetorPublico.R
@@ -4,7 +4,7 @@ divida = BETSget(4536)
 plot(divida)
 
 % elaborar 3 cenarios do comportamento da divida publica de 2017 ate 2021 %
-%considerar a fórmula dt=dt-1*(1+it)/((1+q)*(1+π))-h-s %
+% considerar a fórmula dt=dt-1*(1+it)/((1+q)*(1+π))-h-s %
 % dt --> dívida pública/PIB no ano t;dt-1 --> dívida pública/PIB no anto t-1; it --> taxa nominal média ponderada dos juros da dívida pública no ano t;q --> taxa de crescimento real do PIB;π --> taxa de inflação;h --> superávit primário/pib;s=0. %
 % https://www.itau.com.br/itaubba-pt/analises-economicas/projecoes/longo-prazo-setembro-2018%
 % tem algum código pra puxar desse site as informaçoes?%
