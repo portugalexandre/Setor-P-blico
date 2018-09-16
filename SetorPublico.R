35186a4a7f23e0de1b0eff0973656e6115a6d870
portugalexandre <43226664+portugalexandre@users.noreply.github.com>
2018-09-15 22:23:10 -0300

    Update SetorPublico.R

--- SetorPublico.R
+++ SetorPublico.R
@@ -9,7 +9,5 @@ plot(divida)
 % https://www.itau.com.br/itaubba-pt/analises-economicas/projecoes/longo-prazo-setembro-2018%
 % tem algum código pra puxar desse site as informaçoes?%
 % primeiro cenario – com superávits primários/PIB necessários para manter a dívida constante até 2021 %
-% segundo cenario - com superávits primários/PIB que reduza a dívida em 2% ao finaldos 5 anos;%
-% terceiro cenario - com o déficit primário/PIB 2017 sendo reduzido em 5% do seu total ao ano (p.ex.,
-se em 2017 o déficit primário/PIB foi de 50%, em 2018 deve ser (1 – 0.05) x 50% =
-47,5%).
+% segundo cenario - com superávits primários/PIB que reduza a dívida em 2% ao finaldos 5 anos %
+% terceiro cenario - com o déficit primário/PIB 2017 sendo reduzido em 5% do seu total ao ano (p.ex.,se em 2017 o déficit primário/PIB foi de 50%, em 2018 deve ser (1 – 0.05) x 50% =47,5%) %
