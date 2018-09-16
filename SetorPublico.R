630859c72d6d6e324896c51b55210bb59bdd2081
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-16 20:02:25 -0300

    mudanças

--- SetorPublico.R
+++ SetorPublico.R
@@ -14,9 +14,10 @@ install.packages("readr", force = TRUE) #se não tiver instalado o pacote, rode E
 library(readr) ## Pacote para ler e salvar bases de dados
 
 dados = data.frame(divida) # Gerando uma estrutura de dados apropriada
+saveRDS(dados, file = "dados.rds") # salvando em formato RData, útil para ter os dados limpos e leves
+
 
 write_csv(dados, path = getwd(), col_names = TRUE) #salvando um arquivo de excel com os dados
-saveRDS(dados, file = "dados.rds") # salvando em formato RData, útil para ter os dados limpos e leves
 
 # para saber onde ele salva o arquivo, rode a linha de código abaixo                                                                                                   
 getwd()
